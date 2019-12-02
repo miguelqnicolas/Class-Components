0b1d7ac6f79f910f400364ba7a40b6d87762e68f
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-12-02 15:29:30 -0500

    added constructor function and organized files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ToDoList from './ToDoList';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+class App extends Component {
+	// you will need a place to store your state in this component.
+	// design `App` to be the parent component of your application.
+	// this component is going to take care of state, and any change handlers you need to work with your state
+	
+	constructor() {
+		super();
+		this.state = {
+			task: '',
+			id: Date.now(),
+			completed: false
+		};
+	};
+
+	// handler functions here
+	// changeHandler = (event) => {
+	// 	this.setState.task = event.target.value;
+	// };
+
+	render() {
+		return (
+			<div>
+				<h2>To Do:</h2>
+				<ToDoList/>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/ToDoList.js
@@ -0,0 +1,15 @@
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+
+import React, {Component} from 'react';
+
+class ToDoList extends Component {
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default ToDoList;
